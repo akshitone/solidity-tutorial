71dfb12705ca713ea518bfa442658ca5dd15632f
akshit.mithaiwala@techbulls.com <akshit.mithaiwala@techbulls.com>
2022-02-28 12:12:08 +0530

    without get method and constructor

--- MyContract.sol
+++ MyContract.sol
@@ -1,21 +1,11 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 contract MyContract {
-    // public variable
-    string value; 
-
-    // constructor
-    constructor() public {
-        value = "Hello, world!";
-    } 
+    // public variable declaration with default value
+    string public value = "Hello World";
 
     // setValue function (string)
     function setValue(string memory _value) public {
         value = _value;
-    } 
-
-    // getValue function (string)
-    function getValue() public view returns (string memory) {
-        return value;
     } 
 }
