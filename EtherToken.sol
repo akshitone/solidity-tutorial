2174d84e583a7f6f2fbc6271ecd33807ae584f1a
akshittechbulls <akshit.mithaiwala@techbulls.com>
2022-03-03 00:36:23 +0530

    changes in token file

--- EtherToken.sol
+++ EtherToken.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.6.0 <0.9.0;
 contract EtherToken {
     mapping(address => uint256) public balances; 
 
-    address wallet;
+    // address wallet;
 
     function buyToken() public {
         balances[msg.sender] += 1;
